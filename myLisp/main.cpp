f6b1e7d8104150ceb92b31a915bef3755fc869a7
Timm Knape <timm.knape@dvag.com>
2015-05-29 11:30:09 +0200

    Initial Commit

---
+++ myLisp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myLisp
+//
+//  Created by Timm Knape on 29.05.15.
+//  Copyright (c) 2015 Timm Knape Softwaretechnik (http://www.kna-st.de). All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

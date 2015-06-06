a0cefc715d988a2e90d055b0fa1db5c4ae4b1caa
Timm Knape <timm@kna-st.de>
2015-06-06 22:35:17 +0200

    added repl

--- myLisp/main.cpp
+++ myLisp/main.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
 
-#include "collector.h"
-#include "pair.h"
-#include "string.h"
+#include "parser.h"
 
 int main(int argc, const char * argv[]) {
-    Collector collector;
-    Pair *a = collector.new_pair(Pair::null(), Pair::null());
-    Pair *b = collector.new_pair(Pair::null(), a);
-    Pair *c = collector.new_pair(new String("abc"), b);
-    collector.release_initial_bondage(b);
-    collector.release_initial_bondage(a);
-    collector.make_root(c);
-    collector.collect();
-    
-    // insert code here...
-    std::cout << "This is myLisp\n";
-    std::cout << *c << "\n";
-    
-    collector.release_root(c);
-    collector.collect();
+
+    std::cout << "This is myLisp" << std::endl << std::endl;
+
+    Parser parser;
+    for (;;) {
+        Element *in = parser.parse(std::cin);
+        if (!in) { break; }
+        Element *out = in;
+        std::cout << *out << std::endl;
+    }
     
     return 0;
 }

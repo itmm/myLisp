cb71302d1fabd14c8c420e7d16f8e5cc5de61811
Timm Knape <timm.knape@dvag.com>
2015-06-19 16:44:45 +0200

    added define unit-tests

--- myLisp/fndefine.h
+++ myLisp/fndefine.h
@@ -8,4 +8,23 @@
 			virtual Ptr apply(Ptr arguments, State &state) override;
 	};
 
+    /*TESTS:
+     *
+     *> #include "number.h"
+     *
+     *+ Parser p;
+     *+ p.eval("(def \"a\" 42)");
+     *
+     *	p.eval("(+ a a")->as_number()->value() == 84
+     *
+     */
+
+    /*TESTS:
+     *
+     *+ Parser p;
+     *
+     *  p.eval("def \"a\" 3)")->as_number()->value() == 3
+     *
+     */
+
 #endif

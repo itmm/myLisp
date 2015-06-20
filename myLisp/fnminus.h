179941e11a360e5b9d10bbdcab462b834e0321af
Timm Knape <timm@kna-st.de>
2015-06-20 15:58:05 +0200

    new todo list

--- myLisp/fnminus.h
+++ myLisp/fnminus.h
@@ -8,6 +8,11 @@
         virtual Ptr apply(Ptr arguments, State &state) override;
     };
 
+	/*	Some strange behavior was added: (-) returns -1 and (- a) returns -a.
+	 *	So - can be used as an unary operator and (+), (-) can be used to mark
+	 *	different states as 0 and -1.
+	 */
+
     /*TESTS:
      *
      *>	#include "number.h"
@@ -17,6 +22,7 @@
      *	p.eval("(-)")->as_number()->value() == -Fractional(1)
      *	p.eval("(- 10 2 3)")->as_number()->value() == 5
      *  p.eval("(- -3)")->as_number()->value() == 3
+     *  p.eval("(- 1/3 2)")->as_number()->value() == -Fractional(5, 3)
      *
      */
 

076d44164e994d5280be3e6f21cb2bcc62059879
Timm Knape <timm@kna-st.de>
2015-08-02 21:14:50 +0200

    bulk import unit-tests

--- myLisp/import_handler_bundle.mm
+++ myLisp/import_handler_bundle.mm
@@ -4,10 +4,32 @@
 
 std::vector<std::string> ImportHandlerBundle::find_paths(const std::string &name) {
     NSString *str = [NSString stringWithUTF8String: name.c_str()];
-    NSString *path = [NSBundle.mainBundle pathForResource: str.stringByDeletingPathExtension ofType: str.pathExtension];
-    if (path) {
-        std::vector<std::string> result;
-        result.push_back(path.UTF8String);
+    NSString *base = str.stringByDeletingPathExtension;
+    NSString *ext = str.pathExtension;
+
+    std::vector<std::string> result;
+    
+    NSRange range = [base rangeOfString: @"*"];
+    if (range.length) {
+        NSString *prefix = [base substringToIndex: range.location];
+        NSString *suffix = [base substringFromIndex: range.location + range.length];
+        
+        NSArray *files = [NSBundle.mainBundle pathsForResourcesOfType:ext inDirectory:nil];
+        for (NSUInteger i = 0, count = files.count; i < count; ++i) {
+            NSString *path = files[i];
+            NSString *b = path.lastPathComponent.stringByDeletingPathExtension;
+            if (prefix.length && ! [b hasPrefix: prefix]) continue;
+            if (suffix.length && ! [b hasSuffix: suffix]) continue;
+            result.push_back(path.UTF8String);
+        }
+    } else {
+        NSString *path = [NSBundle.mainBundle pathForResource: base ofType: ext];
+        if (path) {
+            result.push_back(path.UTF8String);
+        }
+    }
+    
+    if (result.size()) {
         return result;
     } else {
         return ImportHandler::find_paths(name);

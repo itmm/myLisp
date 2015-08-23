a37d264f1bc74b9c1d67cd6d44ea1b47419999c3
Timm Knape <timm@kna-st.de>
2015-08-23 12:35:18 +0200

    don't double evaluate unary functions

--- myLisp/unary.cpp
+++ myLisp/unary.cpp
@@ -2,7 +2,6 @@
 #include "pair.h"
 
 EPtr UnaryFunction::apply_evaled(EPtr arguments, State &state) {
-	arguments = eval_arguments(arguments, state);
 	if (! Element::as_pair(arguments)) return state.error("need one argument");
 	if (Pair::cdr(arguments)) return state.error("too many arguments");
 	return apply_unary(EPtr(Pair::car(arguments), state.collector()), state);
